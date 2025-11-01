e3f1398a7fd1134c8d336f2e5d92848bfca0fbc1
rohitraj3808-droid <rohitraj3808@gmail.com>
2025-11-01 20:58:46 +0530

    Add files via upload

---
+++ Sever.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+const products = [
+  { id: 1, name: 'Laptop', price: 1200 },
+  { id: 2, name: 'Mouse', price: 25 },
+  { id: 3, name: 'Keyboard', price: 45 }
+];
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.listen(5000, () => console.log('Server running on port 5000'));
